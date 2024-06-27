2cfcbdd79b122579aa892479e4290f13156f015d
Lisset Rico <lrico5@uic.edu>
2024-06-27 11:54:42 -0500

    testing another version of water pump code and added menu options

--- main.py
+++ main.py
@@ -7,14 +7,34 @@
     Date Created: 06/26/2024
 
 """
-#hi hello
+
+# import etching as Etching
+import water_pump as Water
+
+opt = ''
+
 # menu options
 
-# etch full grid, not ready yet
-    # tell me your grid size
+print('Welcome to Etch Bot!')
 
-# etch one square but probes aren't aligned
+Water.turn_on()
 
-# etch one square, assuming device setup correctly
-
-# quit and move devices once done aka reset everything for the day
+while opt != 'x':
+    print('Pick an Option: ')
+    if opt == '1':
+        # etch full grid, not ready yet
+            # tell me your grid size
+        Etching.full_grid()
+    elif opt == '2':
+        # etch one square but probes aren't aligned
+        Etching.square_vtwo()
+    elif opt == '3':
+        # etch one square, assuming device setup correctly
+        Etching.square_vone()
+    elif opt == 'x':
+        # quit and move devices once done aka reset everything for the day
+        quit()
+    else:
+        print("Command not valid. Try again.")
+        
+print('Have a good day! :)')
--- water_pump.py
+++ water_pump.py
@@ -12,27 +12,60 @@ import time
 from pinpong.board import Board
 from pinpong.libs.dfrobot_dri0050 import DRI0050 # Import DRI0050 library from libs 
 
-#Board("RPi").begin()  #RPi Linux platform 
-Board("Win").begin() #windows platform
+def turn_on():
+  # #Board("RPi").begin()  #RPi Linux platform 
+  # Board("Win").begin() #windows platform
 
-#pwmd = DRI0050(port="/dev/ttyUSB0") #RPi Linux platform
-pwmd = DRI0050(port="COM3")  #Windows platform
+  # #pwmd = DRI0050(port="/dev/ttyUSB0") #RPi Linux platform
+  # pwmd = DRI0050(port="COM5")  #Windows platform
 
-print("version=0x{:x}, addr=0x{:x}".format(pwmd.get_version(), pwmd.get_addr()))
-print("pid=0x{:x}, vid=0x{:x}".format(pwmd.get_vid(), pwmd.get_pid()))
+  # print("version=0x{:x}, addr=0x{:x}".format(pwmd.get_version(), pwmd.get_addr()))
+  # print("pid=0x{:x}, vid=0x{:x}".format(pwmd.get_vid(), pwmd.get_pid()))
 
-while True:
-  print("\n--------Inital Value------") 
-  print("freq={}, duty={:.2f} enable={}".format(pwmd.get_freq(), pwmd.get_duty(), pwmd.get_enable()))  
+  # while True:
+  #   print("\n--------Inital Value------") 
+  #   print("freq={}, duty={:.2f} enable={}".format(pwmd.get_freq(), pwmd.get_duty(), pwmd.get_enable()))  
 
-  print("--------Set a new value------")  
-  #pwmd.pwm(freq=860,duty=0.82) # freq(183HZ-46875HZ) duty(0%-100%)
-  pwmd.set_freq(860) #(183HZ-46875HZ)
-  pwmd.set_duty(0.82)#(0%-100%)
-  pwmd.set_enable(1)
-  print("freq={}, duty={:.2f} enable={}".format(pwmd.get_freq(), pwmd.get_duty(), pwmd.get_enable()))
+  #   print("--------Set a new value------")  
+  #   #pwmd.pwm(freq=860,duty=0.82) # freq(183HZ-46875HZ) duty(0%-100%)
+  #   pwmd.set_freq(860) #(183HZ-46875HZ)
+  #   pwmd.set_duty(0.82)#(0%-100%)
+  #   pwmd.set_enable(1)
+  #   print("freq={}, duty={:.2f} enable={}".format(pwmd.get_freq(), pwmd.get_duty(), pwmd.get_enable()))
 
-  print("--------Restore to factory settings (366HZ, duty ratio 50%, disable output)-------\n")
-  pwmd.pwm(freq=366,duty=0.5) # freq(183HZ-46875HZ) duty(0%-100%)
+  #   print("--------Restore to factory settings (366HZ, duty ratio 50%, disable output)-------\n")
+  #   pwmd.pwm(freq=366,duty=0.5) # freq(183HZ-46875HZ) duty(0%-100%)
+  #   pwmd.set_enable(0)
+  #   time.sleep(5)
+    
+  # second format
+  
+  Board("Win").begin() #windows platform
+  
+  pwmd = DRI0050(port="COM5")  #Windows platform
+  
+  pwmres = 0.1
+  pwnD = 0
+  
+  while True:
+    pwmres = 0.1
+    pwmD = 0
+    
+    for i in range (0,10):
+      pwmd.set_freq(1000)
+      pwmD = pwmD + pwmres
+      pwmd.set_duty(pwmD)
+      
+      pwmd.set_freq(1)
+      time.sleep(0.5)
+      
+    for i in range(0, 10):
+      pwmd.set_freq(1000)
+      pwmD = pwmD - pwmres
+      pwmd.set_duty(pwmD)
+      
+      pwmd.set_freq(1)
+      time.sleep(0.5)
+  
   pwmd.set_enable(0)
-  time.sleep(5)
+  time.sleep(0.5)
