459e6311a0408a39df3f5118744e55cc1e1b6066
NICK (Signatone PC) <lrico5@uic.edu>
2024-07-01 13:21:29 -0500

    signatone testing

--- signatone_driver.py
+++ signatone_driver.py
@@ -12,6 +12,7 @@
 """
 
 import pyvisa
+from pyvisa import constants
 import time
 
 """
@@ -48,8 +49,16 @@ class Signatone:
         print(rm.list_resources()) # prints out an array list of all available devices
 
         try:
-          device_str = ''
+          device_str = 'TCPIP0::124.51.29.30::9090::SOCKET'
           self.device = rm.open_resource(device_str) # connects directly to siglent device
+          self.device.read_termination = '\n'
+          self.device.write_termination = '\n'
+          # set termination method attributes 
+          self.device.set_visa_attribute(constants.VI_ATTR_SUPPRESS_END_EN, constants.VI_FALSE)
+          self.device.set_visa_attribute(constants.VI_ATTR_SEND_END_EN, constants.VI_TRUE)
+          self.device.set_visa_attribute(constants.VI_ATTR_TERMCHAR_EN, constants.VI_TRUE)
+          self.device.set_visa_attribute(constants.VI_ATTR_FILE_APPEND_EN, constants.VI_FALSE)
+          self.device.query('*IDN?')
           print("Connected: ", self.device.query("*IDN?")) # prints siglents basic info(name, ip, etc)
         except Exception as err:
           print("Cannot connect to Signatone: ", err)
@@ -84,8 +93,20 @@ class Signatone:
     def set_device(self, dev):
         device_str = "SETDEVICE " + dev
         self.device.query(device_str)
+    """
+    get_cap : returns the current x,y,z position of the active device in microns
 
-
+    Args:
+        None
+    Returns:
+        x,y,z coordinatinates
+    Raises: 
+        Exception. Prints error if can't connect to device.
+    """
+    def get_cap(self):
+        device_str = "GETCAP"
+        position = self.device.query(device_str)
+        return position
     """
         move_rel : Moves the current device to the new position in reference to the current position.
 
