5f311f11e7c3623dad2eb16de7c7311a93283892
Lisset Rico <lrico5@uic.edu>
2024-07-01 10:15:53 -0500

    water pump comms and main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
     Menu options for etching.
 
-    Authors: UIC Chicago Tech Circle Team (Fernanda Villalpando)
+    Authors: UIC Chicago Tech Circle Team (Lisset Rico, Fernanda Villalpando)
     Collaborator(s): Argonne National Laboratory (Nazar Delegan, Clayton Devault)
     Date Created: 06/26/2024
 
