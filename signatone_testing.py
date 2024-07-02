d5b78cdc10bf21607e619f527ef800687c2de3a4
juice176 <andreamunoz176@gmail.com>
2024-07-02 15:37:15 -0500

    testing To-do

--- signatone_testing.py
+++ signatone_testing.py
@@ -2,8 +2,11 @@ import signatone_driver as Signatone_Driver
 import unittest
 from io import StringIO
 from contextlib import redirect_stdout
-
+#To-Do - make it to be able to test different devices
+#and make it user-friendly :)
 class SignatoneTest(unittest.TestCase):
+    def setUp(self):
+        self.my_string = 'Device name'
     def testingConnection(self):
         #using redirect_stdout to capture print statements from init function in the Signatone Driver class
         with StringIO() as buffer, redirect_stdout(buffer):
@@ -12,7 +15,9 @@ class SignatoneTest(unittest.TestCase):
         #test is looking inside the print statements from the init function to match string variable
         string = 'Signatone CM400 MTS,SERIAL 22080139,VER 1.5.5'
         #Should print OK
+        #i.e. self.assertIn(self.my_string,output)
         self.assertIn(string,output)
+        print("device connection was successful")
     def testingSetDevice1(self):
         #initiziling driver class
         device = Signatone_Driver.Signatone()
