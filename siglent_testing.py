61ba779f6ea178afb3489ef2238c59649a40e5c3
Andrea Munoz <72665263+juice176@users.noreply.github.com>
2024-07-09 11:38:35 -0500

    Update siglent_testing.py

--- siglent_testing.py
+++ siglent_testing.py
@@ -6,15 +6,15 @@ class SiglentTest(unittest.TestCase):
 		device = Siglent_Driver.Siglent()
 		setCurrent = device.set_curr(1)
 		setVoltage = device.set_volt(1)
-		check =device.getoutput()
+		check = device.getoutput()
 		self.assert(check,set)
 		device.output_off()
 	def testingonnoff(self):
 		device = Siglent_Driver.Siglent()
 		on = device.output_on()
-		setvolt=device.set_volt(1)
+		setvolt = device.set_volt(1)
 		off = device.output_off()
-		check=device.get_output()
+		check = device.get_output()
 		self.assert(0,check)
 if __name__ == "__main__":
     unittest.main()
