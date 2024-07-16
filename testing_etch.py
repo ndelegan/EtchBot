c8dc6edb650926fff25546970565e2f9073e0317
juice176 <andreamunoz176@gmail.com>
2024-07-16 13:22:01 -0500

    etching file to test

--- testing_etch.py
+++ testing_etch.py
@@ -1,10 +1,12 @@
 import functions as func
+import signatone_driver
 import signatone_driver as Signatone
 import siglent_driver as Siglent
 import time
 
 def main():
     filename = None
+    signatone = Signatone.Signatone()
     siglent_var = Siglent.Siglent()
     stop = input("Press e to stop etching:")
     siglent_var.output_on()
@@ -12,7 +14,8 @@ def main():
     siglent_var.get_output()
     while stop != "e":
             time.sleep(20)
-            func.take_image(1,filename)
+            image= func.take_image(1,filename)
+            signatone.save_image(image)
     if stop == "e":
         siglent_var.reset_values()
         siglent_var.output_off()
