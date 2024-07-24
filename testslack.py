12486e24b5671f8777e341c0e7e5ac817baa64b9
NICK (Signatone PC) <lrico5@uic.edu>
2024-07-24 14:34:16 -0500

    config file

--- testslack.py
+++ testslack.py
@@ -4,16 +4,17 @@ import signatone_driver as Signatone
 import functions as Functions
 from dotenv import load_dotenv
 import os 
-
+import config
 
 def main():
     bubbles = 0
-    load_dotenv()
-    bubbles_url = os.getenv("BUBBLE")
-    diamond_url = os.getenv("DIAMOND")
-    print(os.getenv("BUBBLE"))
+    # load_dotenv()
+    # bubbles_url = os.getenv("BUBBLE")
+    # diamond_url = os.getenv("DIAMOND")
+    # print(os.getenv("BUBBLE"))
+    
     if bubbles == 0:
-        Functions.send_slack_message(bubbles_url,"Bubble Obstruction!")
-        Functions.send_slack_message(diamond_url,"Diamond Obstruction!")
+        Functions.send_slack_message(config.Bubbles,"Bubble Obstruction!")
+        Functions.send_slack_message(config.Diamonds,"Diamond Obstruction!")
 if __name__ == "__main__":
     main()
