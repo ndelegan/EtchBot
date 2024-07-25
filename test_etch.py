257186348851167f1c061e0b9b8f4ce39a7cdc22
Lisset Rico <lrico5@uic.edu>
2024-07-25 14:49:56 -0500

    complete pseudo code for gds

--- test_etch.py
+++ test_etch.py
@@ -3,6 +3,7 @@ import signatone_driver as Signatone
 import siglent_driver as Siglent
 import time
 import keyboard
+import numpy as np
 
 def main():
     bubble_url = 'https://hooks.slack.com/services/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'
@@ -19,39 +20,47 @@ def main():
     # siglent.set_curr(4)
     # siglent.get_output()
     
-    coor = func.apply_affine_all_mems()
+    corners = func.calculate_corner_coords(9, 50, 50, 250) # w/out trench 250 microns, w/ 200 microns
+    src_points = np.array(corners)
+    dst_points = np.array([[-12395, -8640], [-12460, -11890], [-15705, -11820]])
     
-    print(coor)
+    matrix = func.get_affine_transform(src_points, dst_points)
+    
+    gds_coor = func.get_mem_coords(9, 50, 50, 250)
+    
+    dev_coor = func.apply_affine_all_mems(matrix, gds_coor, 9)
+    
+    print(dev_coor)
     
     counter = 0
-    while True:
-        curr_time = time.time()
+    # while True:
+    #     curr_time = time.time()
         
-        if curr_time - start_time > 20:
-            counter+=1
-            image = func.take_image(counter)
-            signatone.save_image(image)
-            print("image name:", image)
+    #     if curr_time - start_time > 20:
+    #         counter+=1
+    #         image = func.take_image(counter)
+    #         signatone.save_image(image)
+    #         print("image name:", image)
             
-            start_time = time.time()
+    #         start_time = time.time()
             
-            # time.sleep(5)
-            bubble_count = 1
-            if bubble_count > 0:
-                bubble_count = func.bubble_detect(bubble_count, image)
+    #         # time.sleep(5)
+    #         bubble_count = 1
+    #         if bubble_count > 0:
+    #             bubble_count = func.bubble_detect(bubble_count, image)
 
-                # check if bubbles are gone
-                if bubble_count > 0:
-                    func.send_slack_message(bubble_url, "Bubble Obstruction!")
-                    # water pump
+    #             # check if bubbles are gone
+    #             if bubble_count > 0:
+    #                 func.send_slack_message(bubble_url, "Bubble Obstruction!")
+    #                 # water pump
             
-        if keyboard.is_pressed('q'):
-            # siglent.reset_values()
-            # siglent.output_off()
-            break
+    #     if keyboard.is_pressed('q'):
+    #         # siglent.reset_values()
+    #         # siglent.output_off()
+    #         break
         
     print(counter)
-    func.delete_image(counter)
+    # func.delete_image(counter)
     # siglent.reset_values()
     # siglent.output_off()
     print("end")
